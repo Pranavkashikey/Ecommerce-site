4a34104cb060212c404daf46579be6ddab014143
Pranav Kashikey <pranavkashikey93@gmail.com>
2024-03-09 12:06:18 +0530

    This is online shopping site

---
+++ ecommerce website/client/public/navbar.js
@@ -0,0 +1,6 @@
+$(".bars").click(() => {
+  $(".mobile__nav__fade__and__show__circle").toggleClass("open");
+  $(".mobile__nav__fade__and__show").toggleClass("open");
+  $(".bars").toggleClass("toggle")
+  
+})
