f7bf7ac01831bccb609cd62f534fd11b075cd314
Pranav Kashikey <pranavkashikey93@gmail.com>
2024-03-14 01:52:35 +0530

    this new

--- ecommerce website/client/public/navbar.js
+++ ecommerce website/client/public/navbar.js
@@ -2,5 +2,4 @@ $(".bars").click(() => {
   $(".mobile__nav__fade__and__show__circle").toggleClass("open");
   $(".mobile__nav__fade__and__show").toggleClass("open");
   $(".bars").toggleClass("toggle")
-  
 })
