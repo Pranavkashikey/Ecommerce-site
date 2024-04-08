3161e7019c4a6d80842c2c1e7a376b784baf795a
Pranavkashikey <141570116+Pranavkashikey@users.noreply.github.com>
2024-04-09 03:51:17 +0530

    Delete ecommerce website directory

--- ecommerce website/client/public/navbar.js
+++
@@ -1,5 +0,0 @@
-$(".bars").click(() => {
-  $(".mobile__nav__fade__and__show__circle").toggleClass("open");
-  $(".mobile__nav__fade__and__show").toggleClass("open");
-  $(".bars").toggleClass("toggle")
-})
